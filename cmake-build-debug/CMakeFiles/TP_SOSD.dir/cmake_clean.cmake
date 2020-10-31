ffa47c8dfa081fb5c1fe9350e99f7e3abf9f7eb7
JC <a15314@alunos.ipca.pt>
2020-10-31 08:49:03 +0000

    First commit!

---
+++ cmake-build-debug/CMakeFiles/TP_SOSD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_SOSD.dir/main.c.o"
+  "TP_SOSD"
+  "TP_SOSD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP_SOSD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
